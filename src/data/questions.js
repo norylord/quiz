a4430729b3a20ee8b8545b52f564c0a236a0be33
nrylord <neoforest3@gmail.com>
2022-07-27 16:04:32 +0300

    init

---
+++ src/data/questions.js
@@ -0,0 +1,36 @@
+const questions = [
+    {
+        questionText: 'Защита или облачные решения?',
+        answerOptions: [
+            {answerText: 'Защита', answerWeight: 0},
+            {answerText: 'Облачные решение', answerWeight: 1}
+        ]
+    },
+    {
+        questionText: 'Что нужно защитить?',
+        answerOptions: [
+            {answerText: 'Веб-сайт', answerWeight: 0},
+            {answerText: 'Сетевую инфраструктуру', answerWeight: 1}
+        ]
+    },
+    {
+        questionText: 'Переносите свой проект?',
+        answerOptions: [
+            {answerText: 'Нет', answerWeight: 0},
+            {answerText: 'Да', answerWeight: 1}
+        ]
+    },
+    {
+        questionText: 'Какой у вас проект?',
+        answerOptions: [
+            {answerText: 'Лендинг', answerWeight: 0},
+            {answerText: 'Сайт-визитка', answerWeight: 1},
+            {answerText: 'E-commerce', answerWeight: 1},
+            {answerText: 'СМИ', answerWeight: 1},
+            {answerText: 'Своя инфраструктура', answerWeight: 1},
+            {answerText: 'Высоконагруженный проект', answerWeight: 1},
+        ]
+    }
+]
+
+export default questions
